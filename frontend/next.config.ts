867d3929ab57f3da29ecbdb68d1053f9fe1ed80e
salehamuhammad <salehamuhammad77@gmail.com>
2026-03-24 19:01:32 +0500

    c

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: "C:\\Users\\4STAR\\OneDrive\\Documents\\GitHub\\Catering-Management-System\\frontend",
+  },
 };
 
 export default nextConfig;
